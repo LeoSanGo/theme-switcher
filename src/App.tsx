aa1953f376ef8a3ff0c77a0df819ae79f4033e03
LeoSanGo <souoleo@gmail.com>
2021-05-02 19:02:06 -0300

    Criado tema light e compartilhado na aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { ThemeProvider } from  'styled-components';
+import light from './styles/themes/light';
 
+import GlobalStyle from './styles/global';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <ThemeProvider theme={light}>
+      <div className="App">
+        <GlobalStyle />
+        <Header />
+      </div>
+    </ThemeProvider>
   );
 }
 
